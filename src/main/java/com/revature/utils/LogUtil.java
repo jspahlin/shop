be34a184385a7d9616008d2e0d7250934af375c7
jspahlin <jspahlin@kent.edu>
2018-06-30 23:49:41 -0400

    Fixing code smell in sonarqube

--- src/main/java/com/revature/utils/LogUtil.java
+++ src/main/java/com/revature/utils/LogUtil.java
@@ -5,19 +5,20 @@ import java.sql.Connection;
 import org.apache.log4j.Logger;
 
 public class LogUtil {
-        public static void logException(Exception e, @SuppressWarnings("rawtypes") Class c) { // ignoring warnings here, this is richard's code.
-                Logger log = Logger.getLogger(c);
-                log.error(e.getClass()+": "+e.getMessage());
-                for(StackTraceElement s : e.getStackTrace()) {
-                        log.warn(s.getLineNumber()+": "+s.getClassName());
-                }
-        }
-        public static void rollback(Exception e, Connection conn, @SuppressWarnings("rawtypes") Class c) {
-                logException(e, c);
-                try {
-                        conn.rollback();
-                } catch (Exception e1) {
-                        logException(e1, c);
-                }
-        }
+	private LogUtil() {}; // SonarQube says utility classes should not have public constructors.
+    public static void logException(Exception e, @SuppressWarnings("rawtypes") Class c) { // ignoring warnings here, this is richard's code.
+    	Logger log = Logger.getLogger(c);
+    	log.error(e.getClass()+": "+e.getMessage());
+    	for(StackTraceElement s : e.getStackTrace()) {
+    		log.warn(s.getLineNumber()+": "+s.getClassName());
+    	}
+    }
+    public static void rollback(Exception e, Connection conn, @SuppressWarnings("rawtypes") Class c) {
+    	logException(e, c);
+    	try {
+    		conn.rollback();
+    	} catch (Exception e1) {
+    		logException(e1, c);
+    	}
+    }
 }
