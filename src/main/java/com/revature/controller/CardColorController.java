fc7183a06e2c91a69c6e2d9f35fcdbb237126d22
jspahlin <jspahlin@kent.edu>
2018-07-09 09:50:10 -0400

    Add some colors

--- src/main/java/com/revature/controller/CardColorController.java
+++ src/main/java/com/revature/controller/CardColorController.java
@@ -1,5 +1,7 @@
 package com.revature.controller;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.beans.CardColor;
+import com.revature.beans.CardSet;
 import com.revature.data.CardColorDao;
 
 @Controller
@@ -16,10 +20,22 @@ public class CardColorController {
 	private ObjectMapper om = new ObjectMapper();
 	@Autowired
 	CardColorDao ccd;
+	
+	@PostConstruct
+	public void addSomeColors() {
+		ccd.save(new CardColor(0, "Blue"));
+		ccd.save(new CardColor(0, "White"));
+		ccd.save(new CardColor(0, "Green"));
+		ccd.save(new CardColor(0, "Red"));
+		ccd.save(new CardColor(0, "Black"));
+		ccd.save(new CardColor(0, "Colorless"));
+	}
+	
 	@RequestMapping(value="/card/color", method=RequestMethod.GET)
 	@CrossOrigin(origins="http://localhost:4200")
 	@ResponseBody
 	public String getColorType() throws JsonProcessingException {
+		
 		return om.writeValueAsString(ccd.list());
 	}
 }
