4b9e182bc9a84b2bc68d2bee9056c19fde4fc8ef
zia mohiuddin <zain-sheikh@live.com>
2018-07-10 21:54:33 -0400

    made changes and created Inventory controller

--- src/main/java/com/revature/services/InventoryService.java
+++ src/main/java/com/revature/services/InventoryService.java
@@ -7,6 +7,7 @@ import com.revature.beans.Inventory;
 
 public interface InventoryService {
 	public List<Inventory> list();
+	public Inventory get(int id);
 	
 	public Inventory save(Inventory inventory);
 }
